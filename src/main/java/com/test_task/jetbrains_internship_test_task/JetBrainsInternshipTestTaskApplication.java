e44644d997bb848a22f4ac862559307c9f9c0e17
BogdanSom <bogdansam2006@gmail.com>
2025-09-28 16:55:42 +0200

    Initial commit

---
+++ src/main/java/com/test_task/jetbrains_internship_test_task/JetBrainsInternshipTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.test_task.jetbrains_internship_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetBrainsInternshipTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetBrainsInternshipTestTaskApplication.class, args);
+    }
+
+}
