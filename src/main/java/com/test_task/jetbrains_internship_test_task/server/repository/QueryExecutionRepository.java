aea0c91c6e8b232cf3fab158751184bd2ab0f707
BogdanSom <bogdansam2006@gmail.com>
2025-09-29 22:23:10 +0200

    add QueryExecutionRepository

---
+++ src/main/java/com/test_task/jetbrains_internship_test_task/server/repository/QueryExecutionRepository.java
@@ -0,0 +1,20 @@
+package com.test_task.jetbrains_internship_test_task.server.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class QueryExecutionRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public QueryExecutionRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Map<String, Object>> executeNativeQuery(String query) {
+        return jdbcTemplate.queryForList(query);
+    }
+}
