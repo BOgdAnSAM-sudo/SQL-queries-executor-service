cc3ae861ab46a829e2b3b9b5aa1ffed46e0223ff
BogdanSom <bogdansam2006@gmail.com>
2025-09-29 21:54:16 +0200

    add StoredQueryService

---
+++ src/main/java/com/test_task/jetbrains_internship_test_task/server/service/StoredQueryService.java
@@ -0,0 +1,40 @@
+package com.test_task.jetbrains_internship_test_task.server.service;
+
+import com.test_task.jetbrains_internship_test_task.entity.StoredQuery;
+import com.test_task.jetbrains_internship_test_task.server.repository.StoredQueryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
+public class StoredQueryService {
+
+    private final StoredQueryRepository queryRepository;
+
+    @Autowired
+    public StoredQueryService(StoredQueryRepository queryRepository) {
+        this.queryRepository = queryRepository;
+    }
+
+    public StoredQuery addQuery(String query) throws StoredQueryException {
+        if (query == null) {
+            throw new StoredQueryException("Query cannot be null");
+        }
+        StoredQuery newQuery = new StoredQuery(query);
+        return queryRepository.save(newQuery);
+    }
+
+    public List<StoredQuery> getAllQueries() {
+        return queryRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Optional<StoredQuery> getQueryById(Long id) {
+        return queryRepository.findById(id);
+    }
+
+
+}
