60a182d831265c5ae88a6fb3fc34d33cfe0c862c
BogdanSom <bogdansam2006@gmail.com>
2025-09-30 00:54:15 +0200

    add execution of query by id to QueryExecutionService

--- src/main/java/com/test_task/jetbrains_internship_test_task/server/service/QueryExecutionService.java
+++ src/main/java/com/test_task/jetbrains_internship_test_task/server/service/QueryExecutionService.java
@@ -1,18 +1,22 @@
 package com.test_task.jetbrains_internship_test_task.server.service;
 
+import com.test_task.jetbrains_internship_test_task.entity.StoredQuery;
 import com.test_task.jetbrains_internship_test_task.server.repository.QueryExecutionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class QueryExecutionService {
 
     private final QueryExecutionRepository queryExecutionRepository;
+    private final StoredQueryService storedQueryService;
 
-    public QueryExecutionService(QueryExecutionRepository queryExecutionRepository) {
+    public QueryExecutionService(QueryExecutionRepository queryExecutionRepository, StoredQueryService storedQueryService) {
         this.queryExecutionRepository = queryExecutionRepository;
+        this.storedQueryService = storedQueryService;
     }
 
     public List<List<Object>> executeQuery(String query) {
@@ -24,5 +28,13 @@ public class QueryExecutionService {
         return result.stream()
                 .map(row -> row.values().stream().toList())
                 .toList();
+    }
+
+    public List<List<Object>> executeQueryById(Long id){
+        Optional<StoredQuery> storedQuery = storedQueryService.getQueryById(id);
+        if (!storedQuery.isPresent())
+            return null;
+
+        return executeQuery(storedQuery.get().getQuery());
     }
 }
